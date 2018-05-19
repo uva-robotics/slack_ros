4f3c2d2253e2068fd8cf5ccf590a6a8901c1c6f4
Janne Spijkervet <janne.spijkervet@hotmail.com>
2018-05-19 02:17:05 +0200

    Slack ROS bridge

---
+++ README.md
@@ -0,0 +1,13 @@
+# slack_ros
+Slack ROS bridge for communicating between Slack and ROS.
+
+### Installation
+Add your bot's API token to your environment: SLACK_API_TOKEN
+
+### Usage:
+
+Mention @bot_name to send a message to ROS.
+
+###### Topics:
+/ros_to_slack - Publish messages from ROS to a Slack channel.
+/slack_to_ros - Publish messages from Slack to ROS.
---
+++ src/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+import os
+import time
+import re
+import rospy
+from std_msgs.msg import String
+from slackclient import SlackClient
+
+
+
+
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+class SlackRos():
+
+    def __init__(self):
+        self.slack_client = SlackClient(os.environ.get('SLACK_API_TOKEN'))
+
+        # Default ros-channel
+        self.channel_id = "CATHE3NSJ"
+        self.starterbot_id = None
+
+        rospy.init_node("slack_ros")
+        self.slack_to_ros = rospy.Publisher("slack_to_ros", String, queue_size=10)
+        self.ros_to_slack = rospy.Subscriber("ros_to_slack", String, self.ros_slack_callback)
+
+        self.rate = rospy.Rate(10)
+        self.spin()
+
+    def spin(self):
+        if self.slack_client.rtm_connect(with_team_state=False):
+            print("Slack Bot running.")
+            self.starterbot_id = self.slack_client.api_call("auth.test")["user_id"]
+            while not rospy.is_shutdown():
+                command, channel = self.parse_bot_commands(self.slack_client.rtm_read())
+                if command:
+                    self.handle_command(command, channel)
+                self.rate.sleep()
+        else:
+            print("Connection failed.")
+
+
+    def ros_slack_callback(self, data):
+        if self.channel_id is None:
+            rospy.loginfo("No channel ID")
+            pass
+        else:
+            self.slack_client.api_call("chat.postMessage",
+                channel=self.channel_id,
+                text=data.data)
+
+    def parse_bot_commands(self, slack_events):
+        for event in slack_events:
+            if event["type"] == "message" and not "subtype" in event:
+                user_id, message = self.parse_direct_mention(event["text"])
+                if user_id == self.starterbot_id:
+                    return message, event["channel"]
+        return None, None
+
+    def parse_direct_mention(self, message_text):
+        matches = re.search(MENTION_REGEX, message_text)
+        return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+    def handle_command(self, command, channel):
+        if command == "start":
+            self.channel_id = channel
+            response = "You reset my current channel."
+
+        else:
+            response = "Hello"
+            self.slack_to_ros.publish(String(data=command))
+
+        self.slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=response
+        )
+
+if __name__ == "__main__":
+    try:
+        slack_ros = SlackRos()
+    except rospy.ROSInterruptException:
+            pass
